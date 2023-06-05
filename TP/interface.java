3b65acc445871695840aa1177084ce7b815dd52e
Eduard0803 <eduardo83204gmail.com>
2023-06-05 16:05:39 -0300

    update 'TP/interface.java'

--- TP/interface.java
+++ TP/interface.java
@@ -4,32 +4,34 @@ import java.io.*;
 
 class main{
     public static void main(String[] args){
-        JPanel painel = new JPanel();
-        painel.setLayout(new GridLayout(12, 12));
+        JPanel panel = new JPanel(); // instancia um painel
+        panel.setLayout(new GridLayout(12, 12)); // define o tamanho do painel
 
-        JTextField bufferField = new JTextField();
+        // cria as caixas de texto para os inputs
+        panel.add(new JLabel("Insira os dados"));
 
-        painel.add(new JLabel("Placa do veiculo:"));
-        JTextField placaField = new JTextField();
-        painel.add(placaField);
+        panel.add(new JLabel("Placa do veiculo:")); // exibe o texto ao lado da caixa de 'input'
+        JTextField placaField = new JTextField(); // instancia a caixa de 'input'
+        panel.add(placaField); // adiciona a caixa de 'input' no painel
         
-        painel.add(new JLabel("Data de Entrada:"));
+        panel.add(new JLabel("Data de Entrada:"));
         JTextField dataInField = new JTextField();
-        painel.add(dataInField);
+        panel.add(dataInField);
         
-        painel.add(new JLabel("Horário de Entrada:"));
+        panel.add(new JLabel("Horário de Entrada:"));
         JTextField HorarioInField = new JTextField();
-        painel.add(HorarioInField);
+        panel.add(HorarioInField);
         
-        painel.add(new JLabel("Data de saída:"));
+        panel.add(new JLabel("Data de Saída:"));
         JTextField dataOutField = new JTextField();
-        painel.add(dataOutField);
+        panel.add(dataOutField);
         
-        painel.add(new JLabel("Horário de Saída:"));
+        panel.add(new JLabel("Horário de Saída:"));
         JTextField HorarioOutField = new JTextField();
-        painel.add(HorarioOutField);
-
-        int result = JOptionPane.showConfirmDialog(null, painel, "Estacionamentos", JOptionPane.OK_CANCEL_OPTION);
+        panel.add(HorarioOutField);
+        
+        // define o titulo da janela e exibe a janela
+        int result = JOptionPane.showConfirmDialog(null, panel, "SGE", JOptionPane.OK_CANCEL_OPTION);
         if(result == JOptionPane.OK_CANCEL_OPTION)
         {
             // cria um objeto do tipo 'Acesso'
